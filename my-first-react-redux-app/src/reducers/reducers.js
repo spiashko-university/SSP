4e842327dcf123bccd8e5ef84dcb6e6ef23b3bbc
Siarhei Piashko <peshko.sergey@gmail.com>
2017-12-10 20:04:02 +0300

    added actions and redusers

---
+++ my-first-react-redux-app/src/reducers/reducers.js
@@ -0,0 +1,47 @@
+import { combineReducers } from 'redux'
+import {
+  ADD_TODO,
+  SET_VISIBILITY_FILTER,
+  TOGGLE_TODO,
+  VisibilityFilters,
+} from '../actions/actions';
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ];
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo
+      });
+    default:
+      return state
+  }
+}
+
+function visibilityFilter(state = VisibilityFilters.SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter;
+    default:
+      return state
+  }
+}
+
+const todoApp = combineReducers({
+  visibilityFilter,
+  todos
+});
+
+export default todoApp
