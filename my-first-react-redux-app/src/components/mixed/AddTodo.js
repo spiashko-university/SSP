d97ea43f34158eb71d30443dd0f899be1fab3e62
Siarhei Piashko <peshko.sergey@gmail.com>
2017-12-11 18:22:31 +0300

    some refactor to understand what is going on

--- my-first-react-redux-app/src/components/mixed/AddTodo.js
+++ my-first-react-redux-app/src/components/mixed/AddTodo.js
@@ -1,34 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../../actions/actions'
 
-let AddTodo = ({ dispatch }) => {
-  let input;
-  console.log(dispatch);
-  return (
-      <div>
-        <form
-            onSubmit={e => {
-              e.preventDefault();
-              if (!input.value.trim()) {
-                return
-              }
-              dispatch(addTodo(input.value));
-              input.value = ''
-            }}
-        >
-          <input
-              ref={node => {
-                input = node
+class AddTodo extends Component {
+  render() {
+    let input;
+    console.log(this.props.dispatch);
+    return (
+        <div>
+          <form
+              onSubmit={e => {
+                e.preventDefault();
+                if (!input.value.trim()) {
+                  return
+                }
+                this.props.dispatch(addTodo(input.value));
+                input.value = ''
               }}
-          />
-          <button type="submit">
-            Add Todo
-          </button>
-        </form>
-      </div>
-  )
-};
+          >
+            <input
+                ref={node => {
+                  input = node
+                }}
+            />
+            <button type="submit">
+              Add Todo
+            </button>
+          </form>
+        </div>
+    )
+  }
+}
+
 AddTodo = connect()(AddTodo);
 
 export default AddTodo
--- my-first-react-redux-app/src/components/presentationals/Footer.js
+++ my-first-react-redux-app/src/components/presentationals/Footer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import FilterLink from '../containers/FilterLink'
+import { VisibilityFilters } from '../../actions/actions';
 
 class Footer extends Component {
   render() {
@@ -7,15 +8,15 @@ class Footer extends Component {
         <p>
           Show:
           {' '}
-          <FilterLink filter="SHOW_ALL">
+          <FilterLink filter={VisibilityFilters.SHOW_ALL}>
             All
           </FilterLink>
           {', '}
-          <FilterLink filter="SHOW_ACTIVE">
+          <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>
             Active
           </FilterLink>
           {', '}
-          <FilterLink filter="SHOW_COMPLETED">
+          <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>
             Completed
           </FilterLink>
         </p>
