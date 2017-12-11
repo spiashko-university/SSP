c37eca00ac4b2a3e55d6b6da0a72b52c7690592c
Siarhei Piashko <peshko.sergey@gmail.com>
2017-12-11 12:14:57 +0300

    ferst redux result

--- my-first-react-redux-app/src/App.js
+++ my-first-react-redux-app/src/App.js
@@ -2,8 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters
+} from './actions/actions'
+import store from './store/store'
+
 class App extends Component {
   render() {
+
+
+// Log the initial state
+    console.log(store.getState())
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+    const unsubscribe = store.subscribe(() =>
+        console.log(store.getState())
+    )
+
+// Dispatch some actions
+    store.dispatch(addTodo('Learn about actions'))
+    store.dispatch(addTodo('Learn about reducers'))
+    store.dispatch(addTodo('Learn about store'))
+    store.dispatch(toggleTodo(0))
+    store.dispatch(toggleTodo(1))
+    store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+// Stop listening to state updates
+    unsubscribe()
+
+
     return (
       <div className="App">
         <header className="App-header">
---
+++ my-first-react-redux-app/src/store/store.js
@@ -0,0 +1,6 @@
+import { createStore } from 'redux'
+import todoApp from '../reducers/reducers'
+
+let store = createStore(todoApp);
+
+export default store
